48adb2775ee7f72e82c9fe1130f28bbe621ba5a2
AbhijitPatil3306 <90688025+AbhijitPatil3306@users.noreply.github.com>
2025-07-27 14:04:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ def main(big_query_client=client):
     load_job = big_query_client.load_table_from_uri(
         uri, table_id, job_config=job_config
     )
-    load_job.result()  
-
+    load_job.result()
     destination_table = big_query_client.get_table(table_id)
     return {"data": destination_table.num_rows}
 
